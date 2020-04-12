22241aeffc2da830fac972c2bf9897f3ad5abfd4
Revati Tekale <revitekale1910@gmail.com>
2020-04-12 15:00:25 +0530

    REFACTORED:test cases as per new classes

--- src/test/java/QuantityMeasurementTest.java
+++ src/test/java/QuantityMeasurementTest.java
@@ -6,41 +6,46 @@ public class QuantityMeasurementTest {
     Measurement measurement = new Measurement();
 
     @Test
-    public void givenFeetValue_WhenEqual_ReturnTrue() {
-        Unit value1 = new Unit(0);
-        Unit value2 = new Unit(0);
-        boolean isEqual = this.measurement.compare(value1, value2);
-        Assert.assertTrue(isEqual);
+    public void givenFeetValue_WhenEqual_ReturnTrue() throws QuantityMeasurementException {
+       EqualityCheck value1 = new EqualityCheck(1.0, Unit.FEET);
+       EqualityCheck value2 = new EqualityCheck(1.0, Unit.FEET);
+       boolean isEqual = measurement.compare(value1, value2);
+       Assert.assertEquals(true, isEqual);
     }
 
     @Test
     public void givenNullValue_WhenNotEqual_ReturnFalse() {
-        Unit value1 = new Unit(0);
-        boolean isEqual = this.measurement.compare(value1, null);
-        Assert.assertFalse(isEqual);
+        EqualityCheck value1 = new EqualityCheck(1.0, Unit.FEET);
+        try {
+           boolean isEqual = measurement.compare(value1, null);
+        } catch (QuantityMeasurementException e) {
+            Assert.assertEquals(QuantityMeasurementException.ExceptionType.NULL_POINTER_EXCEPTION, e.type);
+        }
     }
 
     @Test
-    public void givenSameReferance_WhenEqual_ReturnTrue() {
-        Unit value1 = new Unit(0);
-        boolean isEqual = this.measurement.compare(value1, value1);
-        Assert.assertTrue(isEqual);
+    public void givenSameReferance_WhenEqual_ReturnTrue() throws QuantityMeasurementException {
+        EqualityCheck value1 = new EqualityCheck(1.0, Unit.FEET);
+        boolean isEqual = measurement.compare(value1, value1);
+        Assert.assertEquals(true, isEqual);
     }
 
     @Test
-    public void givenSameType_WhenEqual_ReturnTrue() {
-        Unit value1 = new Unit(0);
-        Unit value2 = new Unit(0);
-        boolean isEqual = this.measurement.compare(value1, value2);
-        Assert.assertTrue(isEqual);
+    public void givenSameType_WhenEqual_ReturnTrue() throws QuantityMeasurementException {
+        double feet1 = 1.0;
+        double feet2 = 1.0;
+        EqualityCheck value1 = new EqualityCheck(feet1, Unit.FEET);
+        EqualityCheck value2 = new EqualityCheck(feet2, Unit.FEET);
+        boolean isEqual = measurement.compare(value1, value2);
+        Assert.assertEquals(true, isEqual);
     }
 
     @Test
-    public void givenSameFeetValue_WhenEqual_ReturnTrue() {
-        Unit value1 = new Unit(12);
-        Unit value2 = new Unit(12);
-        boolean isEqual = this.measurement.compare(value1, value2);
-        Assert.assertTrue(isEqual);
+    public void givenSameFeetValue_WhenEqual_ReturnTrue() throws QuantityMeasurementException {
+        EqualityCheck value1 = new EqualityCheck(12.0, Unit.FEET);
+        EqualityCheck value2 = new EqualityCheck(12.0, Unit.FEET);
+        boolean isEqual = measurement.compare(value1, value2);
+        Assert.assertEquals(true, isEqual);
     }
 
 }
