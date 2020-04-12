5f695734c6d20926a35704839325ff0715fa8c23
Revati Tekale <revitekale1910@gmail.com>
2020-04-12 14:57:06 +0530

    ADDED:Exception class to handle null pointer exception

---
+++ src/main/java/QuantityMeasurementException.java
@@ -0,0 +1,13 @@
+public class QuantityMeasurementException extends Exception {
+
+    public ExceptionType type;
+
+    public QuantityMeasurementException(ExceptionType type, String message) {
+        super(message);
+        this.type = type;
+    }
+
+    public enum ExceptionType {
+        NULL_POINTER_EXCEPTION
+    }
+}
